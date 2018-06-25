a49977e2e71daed18927c22837e827c68325e52f
Jeffrey Lou <jeffzlou@gmail.com>
2018-06-24 20:27:12 -0400

    logging implemented

--- recording.py
+++ recording.py
@@ -2,6 +2,7 @@
 """Simple logging weather station logic"""
 import sys
 import time
+import logging
 from datetime import datetime
 from influxdb import InfluxDBClient
 import Adafruit_DHT
@@ -10,7 +11,7 @@ if len(sys.argv) > 1:
     LOCATION = sys.argv[1]
 else:
     LOCATION = 'basement'
-print("using", LOCATION, "as location")
+logging.info("using", LOCATION, "as location")
 
 CLIENT = InfluxDBClient(host="localhost", port=8086)
 CLIENT.switch_database("weather")
@@ -21,7 +22,7 @@ PIN = 22
 def write_point():
     """Writes the point to influx"""
     humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
-    print("Temperature:", temperature, "Humidity:", humidity)
+    logging.info("Recording temp:", temperature, "hum:", humidity)
     measurement_json = [
         {
             "measurement": "weather",
@@ -38,6 +39,9 @@ def write_point():
     CLIENT.write_points(measurement_json)
 
 while True:
-    write_point()
+    try:
+        write_point()
+    except:
+        logging.warning("Something bad happened at", datetime.now().toString())
     print("Wrote data to tsdb")
     time.sleep(60)
