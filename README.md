b18b9a15a44662ca48187c7ed6f86e964cfd9494
Jeff Lou <jeffzlou@gmail.com>
2020-11-14 15:57:32 -0500

    slight refactor. Attempt to solve the crashing because of SSL failure (see log)

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # weather-station
 
 Uses a Raspberry pi, with DHT22 sensor to record data to influx db
+
+# usage
+
+Use tmux via raspberry pi and ssh
+
+1. Modify the python program to capture sensor data from the correct pin on the RPi.
+1. Start a tmux session in the pi (`tmux a`)
+2. run weather station using python (`python3 thingspeak_main.py`)
+3. If you're running a local tmux session, detach the pi session using ctrl+b ctrl+b `d`.
+4. All set.
--- thingspeak_main.py
+++ thingspeak_main.py
@@ -9,8 +9,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-location = os.getenv("LOCATION")
-print(f"using {location} as location",)
+sleep_interval = 3 * 60
+exception_interval = 60 * 60
+location = os.getenv("LOCATION") || "Unknown"
+print(f"using {location} as location")
 
 api_key = os.getenv("THINGSPEAK_API_KEY")
 base_url = os.getenv("THINGSPEAK_URI")
@@ -23,28 +25,29 @@ def write_point_wait():
     """Writes the point to influx"""
     humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
     if humidity is None or temperature is None:
-        print("received invalid measurement (None)")
-        return
+        print("Received invalid measurement: None")
+        return False
     if humidity > 100 or humidity < 0:
-        print("Sensor humidity out of range", humidity)
-        return
+        print("Sensor humidity out of range:", humidity)
+        return False
     if temperature > 100 or temperature < 0:
-        print("Nonsensical value from temperature sensor", temperature)
-        return
+        print("Nonsensical value from temperature sensor:", temperature)
+        return False
 
-    temperature = temperature * 1.8 + 32
-    print(f"Recording temp: {temperature}, hum: {humidity}",)
-
-    url = base_url + api_key + f"&field1={temperature}&field2={humidity}"
-    r = requests.get(url)
-    print(f"Status code: {r.status_code}")
-
-    time.sleep(3 * 60)
+    temperature_f = temperature * 1.8 + 32
+    print(f"Measured: {temperature_f}, hum: {humidity}",)
+    return f"&field1={temperature_f}&field2={humidity}"
 
 
 if __name__ == "__main__":
     while True:
         try:
-            write_point_wait()
+            write_string = write_point_wait()
+            if write_string:
+                url = base_url + api_key + write_string
+                r = requests.get(url)
+                print(f"Write attempt status code: {r.status_code}")
+            time.sleep(sleep_interval)
         except Exception:
-            raise RuntimeError("Failed in main loop")
+            # if things go sideways, try again later
+            time.sleep(exception_interval)
